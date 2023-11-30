f830cf8526a7f67cd0e98a5b03badfef80d12d6c
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-11-30 22:01:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,6 @@ async def lusifilms_start():
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
-    await super().start()
     await Media.ensure_indexes()
     me = await Lusi_Films_Bot.get_me()
     temp.ME = me.id
