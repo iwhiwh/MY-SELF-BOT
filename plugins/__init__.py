9046d28fbd047cc81be3b3419174866b65cff608
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-11-30 22:34:44 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,14 +1,8 @@
-from aiohttp import web 
-from info import API_ID, API_HASH, BOT_TOKEN
+from aiohttp import web
+from .route import routes
 
-routes = web.RouteTableDef()
-
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("Lusifilms On Tg")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
-    return web_app
+    return web_app 
