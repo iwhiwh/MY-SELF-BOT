fe3887f3d700ffd80b4dcb9b0311c9046e115dc8
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-12-01 11:42:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -9,6 +9,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_B_TN
 from utils import get_settings, save_group_settings
+from asyncio import coroutine
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
